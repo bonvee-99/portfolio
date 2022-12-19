78c0c14ab391c78c6a05cd5e2d10707aeff94014
Ben Vinnick <bvinnick@thinkwell.group.com>
2022-12-18 23:02:02 -0800

    term init with text

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TerminalClient/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TerminalClient from './components/TerminalClient.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TerminalClient
   }
 }
 </script>
