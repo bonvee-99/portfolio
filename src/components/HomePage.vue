40520aaaea18df5c3fe2b4238a4dd3f958fc3f0f
Ben Vinnick <bvinnick@thinkwell.group.com>
2022-12-21 11:37:31 -0800

    v1

---
+++ src/components/HomePage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home-container">
+    <TerminalClient :updateItem="updateItem" :resume="resume"/>
+    <InfoDisplayer :currentItem="currentItem"/>
+  </div>
+</template>
+
+<script>
+import TerminalClient from '@/components/TerminalClient.vue'
+import InfoDisplayer from '@/components/InfoDisplayer.vue'
+
+export default {
+  name: "HomePage",
+  components: {
+    TerminalClient,
+    InfoDisplayer
+  },
+  props: {
+    resume: Object
+  },
+  data() {
+    return {
+      currentItem: { msg: "hello" }
+    }
+  },
+  methods: {
+    updateItem (item) {
+      this.currentItem = item
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables';
+
+.home-container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ src/components/SkillsCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="skills-container">
+    <div v-for="(value, key) in currentItem" :key="key">
+      <h2>{{ key }}</h2>
+      <ul>
+        <li v-for="(item, index) in value" :key="index">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SkillsCard",
+  props: {
+    currentItem: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables';
+
+.skills-container {
+  display: flex;
+  flex-wrap: wrap;
+
+  div {
+    width: 50%;
+  }
+}
+
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  color: $bg-color;
+  background-color: $pill-color;
+  border-radius: 10px;
+  padding: 5px 10px;
+  margin-right: 4px;
+  margin-bottom: 8px;
+  text-align: center;
+}
+</style>
